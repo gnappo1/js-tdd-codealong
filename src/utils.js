9aa7b989d8cfb43f7cc36e3bfb8d772b330e9296
Matteo Piccini <gnappo.1@live.it>
2023-07-27 08:26:54 -0700

    done

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
 // Your code here
+
+const currentAgeForBirthYear = (year) => {
+    const currentYear = new Date().getFullYear();
+    return currentYear - year;
+}
+
+export { currentAgeForBirthYear };
